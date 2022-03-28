1b6404e2b2d76ad1a16277997a3c29ae60b7405c
Viktor <vsirotkin15@gmail.com>
2022-03-28 09:33:05 +0300

    replaced loader thru render

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,18 @@
 from django.template import loader
+from django.shortcuts import render
 from django.http.response import HttpResponse
 from .models import Bb
 
 # Create your views here.
+# option for render
+def index(request):
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    
+    return render(request, 'bboard/index.html', context=context)
+
+'''
+option1 with loader
 def index(request):
     template    = loader.get_template('bboard/index.html')
     bbs         = Bb.objects.order_by('-published')
@@ -11,3 +21,5 @@ def index(request):
     }
         
     return HttpResponse(template.render(context, request))
+'''
+
