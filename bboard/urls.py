582b97e0ce3579cf8eb939417f64bfb47fcc3c71
Viktor <vsirotkin15@gmail.com>
2022-03-28 16:47:44 +0300

    replaced bboard thru index in index.html

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path
-from .views import by_rubric
+
 from bboard import views
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
-    path('', views.index, name='home'),
+    path('<int:rubric_id>/', views.by_rubric, name='by_rubric'),
+    path('', views.index, name='index'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -21,10 +21,12 @@ def by_rubric(request, rubric_id):
     bbs             = Bb.objects.filter(rubric=rubric_id)
     rubrics         = Rubric.objects.all()
     current_rubric  = Rubric.objects.get(pk=rubric_id)
+    
     context         = {
         'bbs': bbs,
         'rubrics': rubrics,
         'current_rubric': current_rubric}
+    
     return render(request, 'bboard/by_rubric.html', context=context)
 
 '''
