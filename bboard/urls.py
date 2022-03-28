8bf2e9969d0210eb764c1d5b7a9b24cbb09424f8
Viktor <vsirotkin15@gmail.com>
2022-03-28 20:05:48 +0300

    added create.html
    added form in index.html

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
 
 from bboard import views
+from .views import BbCreateView
 
 urlpatterns = [
     path('<int:rubric_id>/', views.by_rubric, name='by_rubric'),
     path('', views.index, name='index'),
+    path('add/', BbCreateView.as_view(), name='add'),
 ]
