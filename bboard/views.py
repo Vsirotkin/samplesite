1803852ed16b3af983b88b7a644ff538c7e77bf3
Viktor <vsirotkin15@gmail.com>
2022-03-28 07:41:07 +0300

    new view for ad

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Bb
 
 # Create your views here.
 def index(request):
-    return HttpResponse('ads emerge here!')
+    s = 'Список объявлений\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s += f'{bb.title} "\r\n" {bb.content} "\r\n\r\n"'
+        
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
